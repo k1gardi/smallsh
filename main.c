44c16d3702836312f59cdaae5c0f53a7752966d6
Kaewan Gardi <k1gardi@gmail.com>
2022-01-31 16:03:18 -0600

    add built in exit and buildcommand

--- main.c
+++ main.c
@@ -16,118 +16,112 @@
 #include <time.h>
 #include <unistd.h>
 
-#define MAX_NUM_LANGS 5
-#define MAX_LANG_LENGTH 20
-#define PREFIX "movies_"
-#define FILETYPE ".csv"
-#define ONID "gardik"
+#define MAX_LINE_LEN 2048
+#define MAX_NUM_ARGS 512
 
-int movieCount; // Points to the number of movies pulled from the data-file
-
-/* This struct holds movie data as a node in part of a linked list*/
-struct movie
+struct command
 {
-	char *title;
-	int year;
-	char languages[MAX_NUM_LANGS][MAX_LANG_LENGTH + 1];
-	char *rating;
-	struct movie *next;
+	char *name;
+	char arguments[MAX_NUM_ARGS][256];
+	int numArgs;
+	char *inDirect;
+	char *outDirect;
+	int processId;
+	int exitStatus;
 };
 
 // Function prototypes
-void sortedInsert(struct movie **, struct movie *);
-void sortByYear(struct movie **);
-struct movie *createMovie(char *);
-struct movie *compileMoviesFromFile(char *);
-void freeMovies(struct movie *);
-char *findLargestFile(void);
-char *findSmallestFile(void);
-void processFile(char *fileName);
-char *generateDirName(void);
-void writeMoviesByYear(struct movie *, char *);
-bool verifyFilename(char *);
+bool isCommentOrBlank(char *);
+struct command *buildCommand(char *);
 
 /* This program reads movie data from a csv and loads it into a linked list.
 * The user is then given multiple options to filter/display the data
 */
 int main(void)
 {
-	int menuSelection;
-	char *fileName = calloc(256, sizeof(char));
+	char *HOME = getenv("HOME");
+	int exitStatus = 0;
+	DIR *currDir = opendir(".");
+
+	// Execute main loop, maintaining shell prompting/reading
 	while (true)
 	{
-		// Present initial options and get choice
-		printf("1. Select file to process\n");
-		printf("2. Exit the program\n");
-		printf("\nEnter a choice 1 or 2: ");
-		scanf("%d", &menuSelection);
+		printf(": ");
+		fflush(stdout);
+		char *buff = NULL;
+		size_t bufflen;
+		getline(&buff, &bufflen, stdin);
+		int inputlen = strlen(buff);
 
-		// Validate input
-		if (menuSelection < 1 || menuSelection > 2)
+		if (buff[inputlen - 1] == '\n')
+			buff[inputlen - 1] = '\0';
+
+		// Ignore comment lines (beginning with '#') -> continue
+		// Ignore blank lines -> continue
+		if (isCommentOrBlank(buff))
 		{
-			printf("Bad Input. Please enter a valid integer choice of 1 or 2.\n\n");
+			free(buff);
+			printf("line was blank or comment\n");
+			fflush(stdout);
+			continue;
+		}
+		// ******************** Built-in Commands ********************
+		// Handle exit command
+		if (strncmp(buff, "exit", 4))
+		{
+			// TODO: Go through my array of child PIDs, kill them if they're running,
+			// and then reach the return EXIT_SUCCESS?
+			free(buff);
+			closedir(currDir);
+			exit(0);
+		}
+
+		// Handle cd
+		struct command *currCommand = buildCommand(buff);
+		// no args -> go to dir specified in HOME env var
+		// Takes path as second arg.  Must support absolute and relative path
+		if (strncmp(buff, "cd", 2))
+		{
+
+			char *newDir = currCommand->arguments[1];
+
+			printf("changing dir to %s", newDir);
+			chdir(newDir);
 			continue;
 		}
 
-		// Exit program
-		if (menuSelection == 2)
-			break;
-
-		// Present second menu and get choice
-		menuSelection = 0;
-		while (true)
+		// Handle status
+		// prints either exit status or terminating signal of last foreground process ran by shell
+		// If this command is run before any foreground command is run, then it should simply return the exit status 0.
+		// The three built-in shell commands do not count as foreground processes for the purposes of this built-in command - i.e., status should ignore built-in commands.
+		else if (strncmp(buff, "status", 6))
 		{
-			printf("\nWhich file you want to process?\n");
-			printf("Enter 1 to pick the largest file\n");
-			printf("Enter 2 to pick the smallest file\n");
-			printf("Enter 3 to specify the name of a file\n");
-			printf("\nEnter a choice from 1 to 3: ");
-			scanf("%d", &menuSelection);
-
-
-			// Load largest file
-			if (menuSelection == 1)
-				fileName = findLargestFile();
-
-			// Load smallest file
-			else if (menuSelection == 2)
-				fileName = findSmallestFile();
-
-			// Get filename from user and verify
-			else if (menuSelection == 3)
-			{
-				printf("Enter the complete file name: ");
-				char *buf = NULL;
-				size_t buflen;
-				getchar();		// Clear newline thats waiting in stdin
-				getline(&buf, &buflen, stdin);
-				int inputlen = strlen(buf);
-
-				if (buf[inputlen-1] == '\n')
-					buf[inputlen-1]  = '\0';
-
-				if (!verifyFilename(buf))
-				{
-					printf("The file %s was not found. Try again\n", buf);
-					continue;
-				}
-				fileName = buf;
-			}
-
-			// Validate filename
-			if (strlen(fileName) == 0)
-			{
-				printf("No file found matching that request. Try again\n");
-				free(fileName);
-				continue;
-			}
-
-			printf("\nNow processing the chosen file named %s\n", fileName);
-			processFile(fileName);
-			break;
+			printf("Exit value %d\n", exitStatus);
+			fflush(stdout);
+			continue;
 		}
+
+		// ******************** Non Built-in Commands ********************
+
+		// Check for <, >, and &
+		// last word '&' means execute command in background. '&' elsewhere -> treat like normal
+		// < and > are used for file redirection after all command args
+
+		// Expand instances of "$$" (dont need to expand other variables)
+
+		// **********************************************************
+		// Non-native commands
+		// Fork a child
+		// Run command with exec
+		// should allow shell scripts to be executed
+		// if no command found in PATH print error message ant exit(1)
+		// Child must terminate after command (for success and fail)
+
+		// ************************************************************
+		// 6. Input & Output Redirection
+		// 7. Executing Commands in Foreground & Background
 	}
-	free(fileName);
+
 	return 0;
 }
 
@@ -138,359 +132,59 @@ int main(void)
 *				Code adapted from https://www.techiedelight.com/given-linked-list-change-sorted-order/ function "sortedInsert"
 *				On date: 01/08/2022
 */
-void sortedInsert(struct movie **head, struct movie *newMovie)
+
+bool isCommentOrBlank(char *line)
 {
-	struct movie temp;
-	struct movie *current = &temp;
-	temp.next = *head;
-
-	// Traverse list until we find the position for the node
-	while (current->next != NULL && current->next->year < newMovie->year)
+	if (strlen(line) == 0)
 	{
-		current = current->next;
-	}
-
-	newMovie->next = current->next;
-	current->next = newMovie;
-	*head = temp.next;
-}
-
-/* Sort the given list by movie year 
-***		Source Citation:
-*				Code adapted from https://www.techiedelight.com/given-linked-list-change-sorted-order/ function "insertSort"
-*				On date: 01/08/2022
-*/
-void sortByYear(struct movie **head)
-{
-	struct movie *result = NULL;   // build the answer here
-	struct movie *current = *head; // iterate over the original list
-	struct movie *next;
-
-	// Keeping track of the head, add each node to the new LL in sorted order
-	while (current != NULL)
-	{
-		next = current->next;
-		sortedInsert(&result, current);
-		current = next;
-	}
-
-	*head = result;
-}
-
-/* Takes a line of movie data and returns a populated movie struct of the data */
-struct movie *createMovie(char *movieData)
-{
-	struct movie *currMovie = malloc(sizeof(struct movie));
-	char *linePtr;
-	char *languagePtr;
-
-	// **************************************************
-	// The first token is the movie title
-	char *token = strtok_r(movieData, ",", &linePtr);
-	currMovie->title = calloc(strlen(token) + 1, sizeof(char));
-	strcpy(currMovie->title, token);
-
-	// **************************************************
-	// The next token is the year
-	token = strtok_r(NULL, ",", &linePtr);
-	currMovie->year = atoi(token);
-
-	// **************************************************
-	// The next token is the languages list
-	// For the first language strip the leading '['
-	linePtr = linePtr + sizeof(char);
-	token = strtok_r(NULL, "]", &linePtr);
-	char *language = strtok_r(token, ";", &languagePtr);
-
-	// Build languages array
-	int langIndex = 0;
-	while (language != NULL || langIndex > 5)
-	{
-		strcpy(currMovie->languages[langIndex], language);
-		language = strtok_r(NULL, ";", &languagePtr);
-		langIndex++;
-	}
-	// strip the trailing ',' after languages
-	linePtr = linePtr + sizeof(char);
-
-	// **************************************************
-	// The last token is the rating
-	token = strtok_r(NULL, "\r", &linePtr);
-	currMovie->rating = calloc(strlen(token) + 1, sizeof(char));
-	strcpy(currMovie->rating, token);
-
-	// **************************************************
-	// Set the next node to NULL in the newly created movie entry
-	currMovie->next = NULL;
-
-	return currMovie;
-}
-
-/* Frees all memory allocated for each movie node */
-void freeMovies(struct movie *currMovie)
-{
-	struct movie *temp;
-
-	while (currMovie != NULL)
-	{
-		temp = currMovie;
-		currMovie = currMovie->next;
-
-		// Free all calloc'd fields then free node
-		free(temp->title);
-		free(temp->rating);
-		free(temp);
-	}
-}
-
-/*
-* Return a linked list of movies by parsing data from each line of the specified file.
-*** 	Source Citation:
-*				Code adapted from the provided sample code at https://replit.com/@cs344/studentsc#student_info1.txt
-*				On date: 01/07/2022
-*/
-struct movie *compileMoviesFromFile(char *fileName)
-{
-	FILE *moviesFile = fopen(fileName, "r");
-
-	// Validate file
-	if (moviesFile == NULL)
-	{
-		printf("Error reading the provided filename.\nPlease enter a valid filename\n");
-		return NULL;
-	}
-
-	char *currLine = NULL;
-	size_t len = 0;
-	ssize_t nread;
-
-	// The head of the linked list
-	struct movie *head = NULL;
-	// The tail of the linked list
-	struct movie *tail = NULL;
-
-	// Advance our pointer past 1st line before entering loop
-	getline(&currLine, &len, moviesFile);
-
-	// Read the file line by line
-	while ((nread = getline(&currLine, &len, moviesFile)) != -1)
-	{
-		// Get a new student node corresponding to the current line
-		struct movie *newNode = createMovie(currLine);
-
-		// Is this the first node in the linked list?
-		if (head == NULL)
-		{
-			// This is the first node in the linked link
-			// Set the head and the tail to this node
-			head = newNode;
-			tail = newNode;
-		}
-		else
-		{
-			// This is not the first node.
-			// Add this node to the list and advance the tail
-			tail->next = newNode;
-			tail = newNode;
-		}
-		movieCount++;
-	}
-	free(currLine);
-	fclose(moviesFile); // Close file
-	return head;
-}
-
-/* Checks if a string ends with ".csv" */
-bool isCSV(char *fileName)
-{
-	size_t fileNameLength = strlen(fileName);
-	size_t fileTypeLength = strlen(FILETYPE);
-	if (fileTypeLength > fileNameLength)
-	{
-		return false;
-	}
-	return strncmp(fileName + fileNameLength - fileTypeLength, FILETYPE, fileTypeLength) == 0;
-}
-/* Returns the string name of the largest csv file in the current directory with the prefix "movies_" 
-* Citation:
-*	Code adapted from exploration example at https://replit.com/@cs344/35statexamplec
-*/
-char *findLargestFile(void)
-{
-	// Prepare to iterate through fileNames
-	int largestSize = 0;
-	char entryName[256];
-	DIR *currDir = opendir(".");
-	struct dirent *aDir;
-	struct dirent *readdir(DIR * dirp);
-	struct stat dirStat;
-	int i = 0;
-
-	while ((aDir = readdir(currDir)) != NULL)
-	{
-		// Verify filetype and prefix
-		if (strncmp(PREFIX, aDir->d_name, strlen(PREFIX)) == 0 && isCSV(aDir->d_name))
-		{
-			// Get meta-data for the current entry
-			stat(aDir->d_name, &dirStat);
-
-			// Remember this fileName if its size is greater than the current greatest size
-			if (i == 0 || dirStat.st_size > largestSize)
-			{
-				largestSize = dirStat.st_size;
-				memset(entryName, '\0', sizeof(entryName));
-				strcpy(entryName, aDir->d_name);
-			}
-			i++;
-		}
-	}
-	char *fileName = calloc(256, sizeof(char));
-	strcpy(fileName, entryName);
-	closedir(currDir);
-	return fileName;
-}
-
-/* Returns the string name of the smallest csv file in the current directory with the prefix "movies_" 
-* Citation:
-*	Code adapted from exploration example at https://replit.com/@cs344/35statexamplec
-*/
-char *findSmallestFile(void)
-{
-	// Prepare to iterate through fileNames
-	int smallestSize = 0;
-	char entryName[256];
-	DIR *currDir = opendir(".");
-	struct dirent *aDir;
-	struct dirent *readdir(DIR * dirp);
-	struct stat dirStat;
-	int i = 0;
-
-	while ((aDir = readdir(currDir)) != NULL)
-	{
-		// Verify filetype and prefix
-		if (strncmp(PREFIX, aDir->d_name, strlen(PREFIX)) == 0 && isCSV(aDir->d_name))
-		{
-			// Get meta-data for the current entry
-			stat(aDir->d_name, &dirStat);
-
-			// Remember this fileName if its size is greater than the current greatest size
-			if (i == 0 || dirStat.st_size < smallestSize)
-			{
-				smallestSize = dirStat.st_size;
-				memset(entryName, '\0', sizeof(entryName));
-				strcpy(entryName, aDir->d_name);
-			}
-			i++;
-		}
-	}
-	char *fileName = calloc(256, sizeof(char));
-	strcpy(fileName, entryName);
-	closedir(currDir);
-	return fileName;
-}
-
-/* Verifies that user provided filename exists in the current directory */
-bool verifyFilename(char *filename)
-{
-	FILE *fp;
-	if ((fp = fopen(filename, "r")))
-	{
-		fclose(fp);
 		return true;
 	}
+
+	if (strncmp(line, "#", 1) == 0)
+	{
+		return true;
+	}
+
 	return false;
 }
 
-/* Processes the data in the infile and saves movie data by year into a new directory.
-*		Creates a new directory with name "ONID.movies.XXXXX" where XXXXX is a random int in [0,99999].
-*		Reads data from given csv one line at a time.
-*		Each movie is written into a file in the new directory corresponding to its release year.
-*/
-void processFile(char *infileName)
+struct command *buildCommand(char *input)
 {
-	// Load and sort data from csv
-	struct movie *movieListHead = compileMoviesFromFile(infileName);
-	sortByYear(&movieListHead);
+	struct command *currCommand = malloc(sizeof(struct command));
+	char *argPtr;
 
-	// Create new directory
-	char *outDirName = generateDirName();
-	int verifyDir = mkdir(outDirName, 0750);
-	if (verifyDir != 0)
+	currCommand->inDirect = NULL;
+	currCommand->outDirect = NULL;
+	currCommand->numArgs = 0;
+
+	// Get command name
+	char *token = strtok_r(input, " ", &argPtr);
+	currCommand->name = calloc(strlen(token) + 1, sizeof(char));
+	strcpy(currCommand->name, token);
+
+	// Build argument array into command struct
+	int argIndex = 0;
+	strcpy(currCommand->arguments[argIndex], currCommand->name);
+	argIndex++;
+
+	char *argument = strtok_r(NULL, " ", &argPtr);
+	while (argument != NULL)
 	{
-		printf("failed to create directory with name: %s\n", outDirName);
-		exit(-1);
+		// TODO is this a > or < or $?????  Do the thing
+		currCommand->numArgs++;
+		strcpy(currCommand->arguments[argIndex], argument);
+		argument = strtok_r(NULL, ";", &argPtr);
+		argIndex++;
 	}
 
-	// write movie data to new directory
-	writeMoviesByYear(movieListHead, outDirName);
-	printf("Created directory with name %s\n\n", outDirName);
-	free(outDirName);
-	freeMovies(movieListHead);
+	// Last argument should be null for exec calls
+	// TODO:
+	// currCommand->arguments[argIndex] = NULL;
+
+	return currCommand;
 }
 
-/* Generates a new random directory name*/
-char *generateDirName(void)
+void freeCommand(struct command *oldCommand)
 {
-	// Get random string in range [0, 99999]
-	srandom(time(NULL));
-	int randInt = random() % 100000;
-	char randString[6];
-	sprintf(randString, "%05d", randInt);
-
-	// Build outdir name
-	char *outdirName = calloc(strlen(ONID) + strlen(".movies.") + strlen(randString) + 1, sizeof(char));
-	strcpy(outdirName, ONID);
-	strcat(outdirName, ".movies.");
-	strcat(outdirName, randString);
-
-	return outdirName;
-}
-
-/* Writes all movie titles into files organized by release year.
-	Files are placed into previously created directory "outDirName"
-*/
-void writeMoviesByYear(struct movie *node, char *outDirName)
-{
-	int currYear = 9999;
-	FILE *outfile;
-	bool isOpenFile = false;
-
-	while (node != NULL)
-	{
-		// Do we need to open a new file?
-		if (node->year != currYear)
-		{
-			// Build new file name
-			currYear = node->year;
-			char yearStr[5];
-			sprintf(yearStr, "%04d", currYear);
-			char *fileName = calloc(strlen(outDirName) + 1 + strlen(yearStr) + strlen(".txt") + 1, sizeof(char));
-			strcat(fileName, outDirName);
-			strcat(fileName, "/");
-			strcat(fileName, yearStr);
-			strcat(fileName, ".txt");
-
-			// Close any open file
-			if (isOpenFile)
-			{
-				fclose(outfile);
-			}
-
-			// Open new file with appropriate permissions
-			outfile = fopen(fileName, "a");
-			chmod(fileName, 0640);
-			isOpenFile = true;
-			free(fileName);
-		}
-
-		// Keep writing to open file
-		fprintf(outfile, "%s\n", node->title);
-		node = node->next;
-	}
-
-	// Close any outstanding files and directories
-	if (isOpenFile)
-	{
-		fclose(outfile);
-	}
+	// TODO: Free an old command struct
 }
